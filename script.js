f3157881cb3de4637c72a14aaf3ecc39fad5828c
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-01-22 18:11:15 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// ________________________VARIANTI-1___________________________
-
 let playerOne = prompt(
   "Player 1, enter rock, paper, or scissors:"
 ).toLowerCase();
@@ -30,28 +28,3 @@ function rockPaperScissors(player1, player2) {
   }
 }
 console.log(rockPaperScissors(playerOne, playerTwo));
-// ________________________VARIANTI-2___________________________
-// let playerOne = prompt(
-//   "Player 1, enter rock, paper, or scissors:"
-// ).toLowerCase();
-// let playerTwo = prompt(
-//   "Player 2, enter rock, paper, or scissors:"
-// ).toLowerCase();
-// function rockPaperScissors(player1, player2) {
-//   if (player1 === "rock" && player2 === "paper") {
-//     return " გაიმარჯვა მეორე მოთამაშემ";
-//   } else if (player2 === "rock" && player1 === "paper") {
-//     return " გაიმარჯვა პირველმა მოთამაშემ";
-//   } else if (player1 === "scissors" && player2 === "paper") {
-//     return " გაიმარჯვა პირველმა მოთამაშემ";
-//   } else if (player2 === "scissors" && player1 === "paper") {
-//     return " გაიმარჯვა მეორე მოთამაშემ";
-//   } else if (player1 === "rock" && player2 === "scissors") {
-//     return "გაიმარჯვა პირველმა მოთამაშემ";
-//   } else if (player2 === "rock" && player1 === "scissors") {
-//     return " გაიმარჯვა მეორე მოთამაშემ";
-//   } else {
-//     return "თქვენი თამაში დასრულდა თანაბრად";
-//   }
-// }
-// console.log(rockPaperScissors(playerOne, playerTwo));
